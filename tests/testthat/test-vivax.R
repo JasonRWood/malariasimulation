30cd383546bc3833edb2e80edf4560eece66e673
RJSheppard <rjs11@ic.ac.uk>
2024-06-28 15:11:04 +0100

    antiparasite immunity function shapes the duration of subpatent infection/rate of recovery from subpatent infection. These values are now assigned during the variable creation and calculation of recovery rates.
    In previous iterations I have kept ID in the vivax model, where it acts as antiparasite immunity. For clarity of thought, I've now added IAA and IAM antiparasite immunities, including their decay, boosting, maternal immunity and reset at death.
    
    We must also adjust the prevalence rendering, which for p.f is based on detectability immunity, and for p.v is explicit in the human states. p_detect_lm is also inappropriate for p.v as the lm_detection is explicit.
    
    du, and ID parameters removed for vivax.
    
    Tests are added for du rates and for prevalence rendering.

--- tests/testthat/test-vivax.R
+++ tests/testthat/test-vivax.R
@@ -20,7 +20,7 @@ test_that('Test difference between falciparum and vivax parameter lists', {
   vivax_parameters <- get_parameters(parasite = "vivax")
   
   expect_true(all(names(falciparum_parameters)[!names(falciparum_parameters) %in% names(vivax_parameters)] %in%
-                    c("du","rvm","rva","rb","b0","b1","ib0","kb","theta0","theta1","kv","fv0","av","gammav","iv0","fd0","ad","gammad","d1","id0","kd","ub","uv","gamma1","pvm","init_ivm","init_ib","init_iva")))
+                    c("du","rvm","rva","rb","rid","b0","b1","ib0","kb","theta0","theta1","kv","fv0","av","gammav","iv0","fd0","ad","gammad","d1","id0","kd","ub","uv","ud","gamma1","pvm","init_ivm","init_ib","init_iva","init_id")))
   expect_true(all(names(vivax_parameters[!names(vivax_parameters) %in% names(falciparum_parameters)]) %in%
                     c("dpcr_max","dpcr_min","kpcr","apcr50","init_iaa","init_iam","ra","ua","b","philm_min","philm_max","klm","alm50","ca","f","gammal","init_hyp","kmax")))
 })
@@ -34,8 +34,7 @@ test_that('Test age structure should not change vivax infectivity', {
   vivax_parameters <- get_parameters(
     parasite = "vivax",
     overrides = list(
-      human_population = 1,
-      init_id  = 0.5))
+      human_population = 1))
   
   state_mock <- mockery::mock('A', cycle = T)
   mockery::stub(create_variables, 'initial_state', state_mock)
@@ -60,5 +59,95 @@ test_that('Test age structure should not change vivax infectivity', {
   
   expect_equal(falc_variables$infectivity$get_values(), 0.03785879)
   expect_equal(vivax_variables$infectivity$get_values(), 0.1)
+  
+})
+
+## Test anti-parasite immunity subpatent progression functions
+test_that('Test anti-parasite immunity function', {
+  vivax_parameters <- get_parameters(parasite = "vivax",
+                                     overrides = list(s_proportion = 0,
+                                                      d_proportion = 0,
+                                                      a_proportion = 0,
+                                                      u_proportion = 1,
+                                                      t_proportion = 0))
+  variables <- create_variables(vivax_parameters)
+  index <- variables$state$get_index_of("U")
+  dpcr_min <- vivax_parameters$dpcr_min
+  dpcr_max <- vivax_parameters$dpcr_max
+  apcr50 <- vivax_parameters$apcr50
+  kpcr <- vivax_parameters$kpcr
+  
+  expect_equal(object = anti_parasite_immunity(
+    dpcr_min, dpcr_max, apcr50, kpcr,
+    variables$iaa$get_values(index),
+    variables$iam$get_values(index)),
+    expected = rep(dpcr_max,100))
+  
+  ## Change initial values of ID, and IDM, check they are the same
+  variables$iaa <- individual::DoubleVariable$new(1:100)
+  UAA_durations <- anti_parasite_immunity(
+    dpcr_min, dpcr_max, apcr50, kpcr,
+    variables$iaa$get_values(index),
+    variables$iam$get_values(index))
+  
+  variables$iaa <- individual::DoubleVariable$new(rep(0,100))
+  variables$iam <- individual::DoubleVariable$new(1:100)
+  UAM_durations <- anti_parasite_immunity(
+    dpcr_min, dpcr_max, apcr50, kpcr,
+    variables$iaa$get_values(index),
+    variables$iam$get_values(index))
+  
+  expect_equal(object = UAA_durations, expected = UAM_durations)
+  
+  ## Check convergence to min_du at high immunity
+  variables$iaa <- individual::DoubleVariable$new(rep(1E7,100))
+  variables$iam <- individual::DoubleVariable$new(rep(0,100))
+  expect_equal(object = anti_parasite_immunity(
+    dpcr_min, dpcr_max, apcr50, kpcr,
+    variables$iaa$get_values(index),
+    variables$iam$get_values(index)),
+    expected = rep(dpcr_min,100),
+    tolerance = 1E-2)
+})
+
+test_that('that vivax patent prevalence rendering works', {
+  
+  timestep <- 1
+  state <- individual::CategoricalVariable$new(
+    c('U', 'A', 'D', 'S', 'Tr'),
+    c('U', 'A', 'D', 'D', 'D', 'S')
+  )
+  birth <- individual::IntegerVariable$new(
+    -c(3, 4, 5, 1, 11, 6) * 365
+  )
+  immunity <- individual::DoubleVariable$new(rep(1, 6))
+  vivax_parameters <- get_parameters(parasite = "vivax")
+  renderer <- mock_render(1)
+  
+  process <- create_prevelance_renderer(
+    state,
+    birth,
+    immunity,
+    vivax_parameters,
+    renderer
+  )
+  
+  process(timestep)
+  
+  mockery::expect_args(
+    renderer$render_mock(),
+    1,
+    'n_detect_lm_730_3650',
+    2,
+    timestep
+  )
+  
+  mockery::expect_args(
+    renderer$render_mock(),
+    2,
+    'n_detect_pcr_730_3650',
+    3,
+    timestep
+  )
   
 })
