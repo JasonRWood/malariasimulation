5cc601fb34f1cee9fe1dcfecea095d218bcb33f2
RJSheppard <rjs11@ic.ac.uk>
2024-07-10 15:55:12 +0100

    Replaced falciparum/vivax parameter comparison test that compares precise differences.

--- tests/testthat/test-vivax.R
+++ tests/testthat/test-vivax.R
@@ -19,8 +19,22 @@ test_that('Test difference between falciparum and vivax parameter lists', {
   falciparum_parameters <- get_parameters(parasite = "falciparum")
   vivax_parameters <- get_parameters(parasite = "vivax")
   
-  expect_true(all(names(falciparum_parameters)[!names(falciparum_parameters) %in% names(vivax_parameters)] %in%
-                    c("du","rvm","rva","rb","b0","b1","ib0","kb","theta0","theta1","kv","fv0","av","gammav","iv0","fd0","ad","gammad","d1","id0","kd","ub","uv","gamma1","pvm","init_ivm","init_ib","init_iva")))
-  expect_true(all(names(vivax_parameters[!names(vivax_parameters) %in% names(falciparum_parameters)]) %in%
-                    c("dpcr_max","dpcr_min","kpcr","apcr50","init_iaa","init_iam","ra","ua","b","philm_min","philm_max","klm","alm50","ca","f","gammal","init_hyp","kmax")))
+  in_falciparum_not_vivax <- setdiff(names(falciparum_parameters), names(vivax_parameters))
+  in_vivax_not_falciparum <- setdiff(names(vivax_parameters), names(falciparum_parameters))
+  
+  expect_identical(
+    in_falciparum_not_vivax,
+    character(0)
+  )
+  
+  expect_identical(
+    sort(in_vivax_not_falciparum),
+    c("dpcr_max", "dpcr_min", "k_pcr", "apcr50", # human sub-patent state delay
+      "init_iaa", "init_iam", "ra", "ua", # antiparasite immunity parameters)
+      "b", # probability of infection given an infectious bite
+      "philm_min", "philm_max", "klm", "alm50", # probability of light-microscopy detectable infection parameters
+      "ca", # light-microscopy detectable infection infectivity towards mosquitos
+      "f", "gammal", "init_hyp", "kmax" # hypnozoite parameters
+    )
+  )
 })
